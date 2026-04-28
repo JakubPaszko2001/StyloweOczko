2b05095541b782568bc6ab7bac26baf46b1597c9
P4CHO <jakubpaszko01@gmail.com>
2026-04-28 17:57:35 +0200

    bez formularza

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,10 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+export const dynamic = "force-dynamic";
 
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const { imie, nazwisko, telefon, email, usluga, data, godzina, uwagi } =
     await req.json();
 
