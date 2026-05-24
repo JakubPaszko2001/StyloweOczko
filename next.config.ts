1cb5f8be9fb0066ccca83add057752ed9c12519d
P4CHO <jakubpaszko01@gmail.com>
2026-05-24 15:53:57 +0200

    zmiany

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // <-- Dodaj tę linijkę tutaj
   images: {
-    unoptimized: true, // <-- BARDZO WAŻNE DLA EXPORTU NA ZWYKŁY HOSTING
     remotePatterns: [
       {
         protocol: "https",
